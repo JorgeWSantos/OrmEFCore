e378158f247ca10b9ee150cf10c3847dd19f30d9
Jorge W. Santos <jorgeewillian00@hotmail.com>
2018-10-11 12:21:25 -0300

    Projeto em Console Application

---
+++ uniinfoORM/uniinfoORM/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using uniinfo.Extensions;
+using uniinfoORM.Controller;
+using uniinfoORM.Model;
+
+namespace uniinfoORM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UniinfoContext context = new UniinfoContext();
+            context.LogSQLToConsole();
+
+            foreach( var chamado in context.ChamadosAtendimento)
+            {
+
+            }
+
+        }
+    }
+}
